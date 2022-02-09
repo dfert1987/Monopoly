1803e0c285a3d98d06b7f82031804e228c2a1399
David Fertitta <Davidj.fertitta@gmail.com>
2022-02-08 22:49:46 -0600

    taxes added

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -3,6 +3,7 @@ import Piece1 from "../Assets/Pieces/Piece1.png";
 import Piece2 from "../Assets/Pieces/Piece2.png";
 import ditielogo from "../Assets/PropertyImages/ditielogo.png";
 import jug from "../Assets/PropertyImages/jug.png";
+import homeLink from "../Assets/PropertyImages/homeLink.png";
 import mopAyi from "../Assets/PropertyImages/mopAyi.png";
 import "./Styles/Board.css";
 
@@ -41,6 +42,19 @@ export const Board = ({ counterP1, counterP2, properties }) => {
             {counterP2 === 39 ? (
               <img className="piece two" src={Piece2} alt="p2 game piece" />
             ) : null}
+            <div className="left-rr-container">
+              <div>
+                <h3 className="left-rr agent">Agent Fee</h3>
+              </div>
+              <div className="logo-cont">
+                <img
+                  className="subway-logo-left agent"
+                  src={homeLink}
+                  alt="subway-logo"
+                />
+              </div>
+              <p className="left-rr-price agent">Pay 75rmb</p>
+            </div>
           </div>
           <div id="38" className="left-space">
             <div className="left-main">
@@ -208,6 +222,12 @@ export const Board = ({ counterP1, counterP2, properties }) => {
             {counterP2 === 5 ? (
               <img className="piece two" src={Piece2} alt="p2 game piece" />
             ) : null}
+            <div className="top-rr-container tax">
+              <div>
+                <h3 className="top-rr">RENEW VISA</h3>
+                <p className="top-rr instructions">Pay 10% or $200</p>
+              </div>
+            </div>
           </div>
           <div id="6" className="top-space">
             {counterP1 === 6 ? (
@@ -292,10 +312,10 @@ export const Board = ({ counterP1, counterP2, properties }) => {
           </div>
           <div className="card-container">
             <div className="card">
-              <h4 className="card-title">Community Chest</h4>
+              <h4 className="card-title">Guanxi</h4>
             </div>
             <div className="card">
-              <h4 className="card-title">Chance</h4>
+              <h4 className="card-title">Yunqi</h4>
             </div>
           </div>
         </div>
