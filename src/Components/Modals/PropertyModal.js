f9e285eb155063f288d47315561e0182cccacd4e
David Fertitta <Davidj.fertitta@gmail.com>
2022-02-24 20:42:12 -0600

    property card two

--- src/Components/Modals/PropertyModal.js
+++ src/Components/Modals/PropertyModal.js
@@ -78,11 +78,42 @@ const PropertyModal = ({
       close === false
     ) {
       return (
-        <>
-          <div className={`banner ${onProp2.color}`}>
-            <h2 className="prop-name">{onProp2.Name.toUpperCase()}</h2>
+        <div className="main-card">
+          <div className={`outer-banner`}>
+            <div className={`banner ${onProp2.color}`}>
+              <h2 className="prop-name">{onProp2.Name.toUpperCase()}</h2>
+            </div>
           </div>
-        </>
+          <div className="all-prop-info">
+            <div className="main-prop-info">
+              <h4 className="rent">{`RENT ¥${onProp2.rent}`}</h4>
+              <div className="house-container one">
+                <p className="left-side">With 1 House</p>
+                <p className="right-side">{`¥${onProp2.oneHouse}`}</p>
+              </div>
+              <div className="house-container two">
+                <p className="left-side">With 2 Houses</p>
+                <p className="right-side">{`¥${onProp2.twoHouses}`}</p>
+              </div>
+              <div className="house-container three">
+                <p className="left-side">With 3 Houses</p>
+                <p className="right-side">{`¥${onProp2.threeHouses}`}</p>
+              </div>
+              <div className="house-container four">
+                <p className="left-side">With 4 Houses</p>
+                <p className="right-side">{`¥${onProp2.fourHouses}`}</p>
+              </div>
+              <div className="house-container hotel">
+                <p className="left-side">With Hotel</p>
+                <p className="right-side">{`¥${onProp2.hotel}`}</p>
+              </div>
+            </div>
+            <div className="secondary-prop-info">
+              <h4 className="mortgage">{`Mortgage Value: ¥${onProp2.mortgage}`}</h4>
+              <p className="houses">{`Houses Cost ¥${onProp2.buidlingCost}`}</p>
+            </div>
+          </div>
+        </div>
       );
     }
     return null;
