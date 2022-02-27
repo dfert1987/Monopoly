105d2d93155336380653d4ffac86ff1c3f283c05
David Fertitta <Davidj.fertitta@gmail.com>
2022-02-27 14:13:34 -0600

    unfilled squares set up on modals

--- src/Components/Modals/OwnedProperties.js
+++ src/Components/Modals/OwnedProperties.js
@@ -26,22 +26,142 @@ const OwnedProperties = ({
                   <FontAwesomeIcon className="x-icon" icon={faXmark} />
                 </button>
               </div>
-              <h1 className="title">PROPERTIES</h1>
-              <div className="player-row">
-                <h2 className="player1">PLAYER 1</h2>
-                <h2 className="player2">PLAYER 2</h2>
-              </div>
+              <h1 className="title">HOLDINGS</h1>
               <div className="square-columns">
                 <div className="property-squares">
-                  <div className="card-row brown">
-                    <div className="cardSquare"></div>
-                    <div className="cardSquare"></div>
+                  <div className="player-row">
+                    <h2 className="player1">PLAYER 1</h2>
+                  </div>
+                  <h3 className="subheader">Properties</h3>
+                  <div className="card-row left five">
+                    <div className="card-group twoGroup">
+                      <div className="cardSquare-brown"></div>
+                      <div className="cardSquare-brown"></div>
+                    </div>
+                    <div className="card-group threeGroup">
+                      <div className="cardSquare-light-blue"></div>
+                      <div className="cardSquare-light-blue"></div>
+                      <div className="cardSquare-light-blue"></div>
+                    </div>
+                  </div>
+                  <div className="card-row left six">
+                    <div className="card-group threeGroup">
+                      <div className="cardSquare-violet"></div>
+                      <div className="cardSquare-violet"></div>
+                      <div className="cardSquare-violet"></div>
+                    </div>
+                    <div className="card-group threeGroup">
+                      <div className="cardSquare-orange"></div>
+                      <div className="cardSquare-orange"></div>
+                      <div className="cardSquare-orange"></div>
+                    </div>
+                  </div>
+                  <div className="card-row left six">
+                    <div className="card-group threeGroup">
+                      <div className="cardSquare-red"></div>
+                      <div className="cardSquare-red"></div>
+                      <div className="cardSquare-red"></div>
+                    </div>
+                    <div className="card-group threeGroup">
+                      <div className="cardSquare-yellow"></div>
+                      <div className="cardSquare-yellow"></div>
+                      <div className="cardSquare-yellow"></div>
+                    </div>
+                  </div>
+                  <div className="card-row left five">
+                    <div className="card-group threeGroup">
+                      <div className="cardSquare-green"></div>
+                      <div className="cardSquare-green"></div>
+                      <div className="cardSquare-green"></div>
+                    </div>
+                    <div className="card-group threeGroup">
+                      <div className="cardSquare-blue"></div>
+                      <div className="cardSquare-blue"></div>
+                    </div>
+                  </div>
+                  <h3 className="subheader">Railroads</h3>
+                  <div className="card-row left four">
+                    <div className="card-group fourGroup">
+                      <div className="cardSquare-black"></div>
+                      <div className="cardSquare-black"></div>
+                      <div className="cardSquare-black"></div>
+                      <div className="cardSquare-black"></div>
+                    </div>
+                  </div>
+                  <h3 className="subheader">Utilities</h3>
+                  <div className="card-row left two">
+                    <div className="card-group threeGroup">
+                      <div className="cardSquare-grey"></div>
+                      <div className="cardSquare-grey"></div>
+                    </div>
                   </div>
                 </div>
                 <div className="property-squares">
-                  <div className="card-row brown">
-                    <div className="cardSquare"></div>
-                    <div className="cardSquare"></div>
+                  <div className="player-row">
+                    <h2 className="player2">PLAYER 2</h2>
+                  </div>
+                  <h3 className="subheader">Properties</h3>
+                  <div className="card-row right five">
+                    <div className="card-group twoGroup">
+                      <div className="cardSquare-brown"></div>
+                      <div className="cardSquare-brown"></div>
+                    </div>
+                    <div className="card-group threeGroup">
+                      <div className="cardSquare-light-blue"></div>
+                      <div className="cardSquare-light-blue"></div>
+                      <div className="cardSquare-light-blue"></div>
+                    </div>
+                  </div>
+                  <div className="card-row left six">
+                    <div className="card-group threeGroup">
+                      <div className="cardSquare-violet"></div>
+                      <div className="cardSquare-violet"></div>
+                      <div className="cardSquare-violet"></div>
+                    </div>
+                    <div className="card-group threeGroup">
+                      <div className="cardSquare-orange"></div>
+                      <div className="cardSquare-orange"></div>
+                      <div className="cardSquare-orange"></div>
+                    </div>
+                  </div>
+                  <div className="card-row left six">
+                    <div className="card-group threeGroup">
+                      <div className="cardSquare-red"></div>
+                      <div className="cardSquare-red"></div>
+                      <div className="cardSquare-red"></div>
+                    </div>
+                    <div className="card-group threeGroup">
+                      <div className="cardSquare-yellow"></div>
+                      <div className="cardSquare-yellow"></div>
+                      <div className="cardSquare-yellow"></div>
+                    </div>
+                  </div>
+                  <div className="card-row left five">
+                    <div className="card-group threeGroup">
+                      <div className="cardSquare-green"></div>
+                      <div className="cardSquare-green"></div>
+                      <div className="cardSquare-green"></div>
+                    </div>
+                    <div className="card-group threeGroup">
+                      <div className="cardSquare-blue"></div>
+                      <div className="cardSquare-blue"></div>
+                    </div>
+                  </div>
+                  <h3 className="subheader">Railroads</h3>
+                  <div className="card-row left four">
+                    <div className="card-group fourGroup">
+                      <div className="cardSquare-black"></div>
+                      <div className="cardSquare-black"></div>
+                      <div className="cardSquare-black"></div>
+                      <div className="cardSquare-black"></div>
+                    </div>
+                  </div>
+                  <h3 className="subheader">Utilities</h3>
+                  <div className="card-row left two">
+                    <div className="card-group threeGroup">
+                      <div className="cardSquare-grey"></div>
+                      <div className="cardSquare-grey"></div>
+                    </div>
                   </div>
                 </div>
               </div>
