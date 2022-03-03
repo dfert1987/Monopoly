5d2d68f8e17295e334d756fd31554bdcadbf26de
David Fertitta <Davidj.fertitta@gmail.com>
2022-03-03 10:15:12 -0600

    add roll function to pay util

--- src/Components/Modals/PayOpponentUtil.js
+++ src/Components/Modals/PayOpponentUtil.js
@@ -2,6 +2,12 @@ import React, { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import dice1 from "../../Assets/Dice/dice1.png";
+import dice2 from "../../Assets/Dice/dice2.png";
+import dice3 from "../../Assets/Dice/dice3.png";
+import dice4 from "../../Assets/Dice/dice4.png";
+import dice5 from "../../Assets/Dice/dice5.png";
+import dice6 from "../../Assets/Dice/dice6.png";
 import "../Styles/PayOpponentModal.css";
 
 export const PayOpponentUtil = ({
@@ -18,6 +24,11 @@ export const PayOpponentUtil = ({
   setP2Money,
 }) => {
   const [rent, setRent] = useState();
+  const [die1, setDie1] = useState(1);
+  const [die2, setDie2] = useState(1);
+  const [die1Img, setDie1Img] = useState(dice1);
+  const [die2Img, setDie2Img] = useState(dice1);
+  const [multiplier, setMultiplier] = useState(4);
   const backdrop = {
     visible: { opacity: 1 },
     hidden: { opacity: 0 },
@@ -42,6 +53,89 @@ export const PayOpponentUtil = ({
     setRent(null);
   };
 
+  const rollDice = (e) => {
+    e.preventDefault();
+    let min = 1;
+    let max = 6;
+    let first = Math.floor(Math.random() * (max - min + 1)) + min;
+    let second = Math.floor(Math.random() * (max - min + 1)) + min;
+    setDie1(first);
+    setDie2(second);
+    dieImages1(first, "one");
+    dieImages2(second, "one");
+    if (multiplier === 4) {
+      let totalDue = (first + second) * 4;
+      setRent(totalDue);
+    } else if (multiplier === 10) {
+      let totalDue = (first + second) * 10;
+      setRent(totalDue);
+    }
+  };
+
+  const dieImages1 = (first, pair) => {
+    if (pair === "one") {
+      if (first === 1) {
+        setDie1Img(dice1);
+      }
+      if (first === 2) {
+        setDie1Img(dice2);
+      }
+      if (first === 3) {
+        setDie1Img(dice3);
+      }
+      if (first === 4) {
+        setDie1Img(dice4);
+      }
+      if (first === 5) {
+        setDie1Img(dice5);
+      }
+      if (first === 6) {
+        setDie1Img(dice6);
+      }
+    }
+  };
+
+  const dieImages2 = (second, pair) => {
+    if (pair === "one") {
+      if (second === 1) {
+        setDie2Img(dice1);
+      }
+      if (second === 2) {
+        setDie2Img(dice2);
+      }
+      if (second === 3) {
+        setDie2Img(dice3);
+      }
+      if (second === 4) {
+        setDie2Img(dice4);
+      }
+      if (second === 5) {
+        setDie2Img(dice5);
+      }
+      if (second === 6) {
+        setDie2Img(dice6);
+      }
+    }
+  };
+
+  const propName = () => {
+    if (onUtil) {
+      return onUtil.Name;
+    } else if (onUtil2) {
+      return onUtil2.Name;
+    }
+    return null;
+  };
+
+  const player = () => {
+    if (payUtilTo && payUtilTo === 2) {
+      return "Player 2";
+    } else if (payUtilTo && payUtilTo === 1) {
+      return "Player 1";
+    }
+    return null;
+  };
+
   return (
     <AnimatePresence exitBeforeEnter>
       {payUtil === true && (onUtil || onUtil2) ? (
@@ -58,7 +152,44 @@ export const PayOpponentUtil = ({
             initial="hidden"
             animate="visible"
             exit="hidden"
-          ></motion.div>
+          >
+            <div className="button-row">
+              <button className="close-button" onClick={handleClose}>
+                <FontAwesomeIcon className="x-icon" icon={faXmark} />
+              </button>
+            </div>
+            <div className="main-content-container">
+              <h2 className="line-1">{`${player()} Owns ${propName()}`}</h2>
+              {/* {cashPic()} */}
+              <h3 className="roll-instruction">
+                Roll dice to determine what you owe.
+              </h3>
+              <p className="explanation">Pay 4x the amount on the die</p>
+              <div className="p1-dice-button">
+                <div className="roll-section">
+                  {die1 && die2 ? (
+                    <>
+                      {die1Img ? (
+                        <img className="die" src={die1Img} alt="die" />
+                      ) : null}
+                      {die2Img ? (
+                        <img className="die" src={die2Img} alt="die" />
+                      ) : null}
+                    </>
+                  ) : null}
+                </div>
+                <div className="button-container">
+                  <button onClick={rollDice} className="roll-button">
+                    ROLL DICE
+                  </button>
+                </div>
+              </div>
+              <h2 className="ammount">
+                Pay <span className="rent">{`¥${rent}`}</span> in Rent.
+              </h2>
+              {/* <h4 className="pay-saying">{saying()}</h4> */}
+            </div>
+          </motion.div>
         </motion.div>
       ) : null}
     </AnimatePresence>
