d361093f85017049bd0d7b145522946ed3af90e8
David Fertitta <Davidj.fertitta@gmail.com>
2022-03-27 14:53:18 -0500

    now resets cards if all finished

--- src/Components/Modals/Cards/InnerCard.js
+++ src/Components/Modals/Cards/InnerCard.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Guanxi } from "../../../Assets/Cards/Guanxi/Guanxi";
 import { Yunqi } from "../../../Assets/Cards/Yunqi/Yunqi";
 import "../../Styles/Card.css";
 
@@ -15,48 +16,43 @@ const InnerCard = ({
   const [currentCard, setCurrentCard] = useState();
 
   const arrayRemovePre = () => {
-    console.log("hi");
     if (cardOption === "GUANXI") {
-      console.log("ok");
       arrayRemove(guanxi, currentCard);
     }
     if (cardOption === "YUNQI") {
-      console.log("ok yunqi");
       arrayRemove(yunqi, currentCard);
     }
   };
 
   const arrayRemove = (arr, value) => {
     if (arr === guanxi) {
-      console.log("ok 2");
       let newArray = arr.filter(function (ele) {
         return ele !== value;
       });
       setGuanxi(newArray);
-      // console.log(newArray);
     } else if (arr === yunqi) {
-      console.log("ok 3");
       let newArray = arr.filter(function (ele) {
         return ele !== value;
       });
-
-      // console.log(newArray);
-
       setYunqi(newArray);
-      console.log(Yunqi);
     }
   };
-
-  console.log(guanxi);
-  console.log(yunqi);
 
   useEffect(() => {
     if (cardOption === "GUANXI" && guanxi.length) {
       let cardChoice = guanxi[Math.floor(Math.random() * guanxi.length)];
       setCurrentCard(cardChoice);
+    } else if (cardOption === "GUANXI" && !guanxi.length) {
+      setGuanxi(Guanxi);
+      let cardChoice = guanxi[Math.floor(Math.random() * guanxi.length)];
+      setCurrentCard(cardChoice);
     } else if (cardOption === "YUNQI" && yunqi.length) {
       let cardChoice = yunqi[Math.floor(Math.random() * yunqi.length)];
       setCurrentCard(cardChoice);
+    } else if (cardOption === "YUNQI" && !yunqi.length) {
+      setYunqi(Yunqi);
+      let cardChoice = yunqi[Math.floor(Math.random() * yunqi.length)];
+      setYunqi(cardChoice);
     }
     return null;
   }, [cardOption, guanxi, yunqi]);
