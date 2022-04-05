19fd849fc9eee7ec737df29c3fb985589d658f6f
David Fertitta <Davidj.fertitta@gmail.com>
2022-04-05 10:06:09 -0500

    router installed. new files created

---
+++ src/SetupPages/ChoosePieces.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const ChoosePieces = () => {
+  return (
+    <div className="page-container">
+      <h1 className="main-title">Choose Your Piece!</h1>
+    </div>
+  );
+};
+
+export default ChoosePieces;
---
+++ src/SetupPages/Landing.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Landing = () => {
+  return (
+    <div className="page-container">
+      <h1 className="main-title">Welcome to Laowai Monopoly!</h1>
+    </div>
+  );
+};
+
+export default Landing;
