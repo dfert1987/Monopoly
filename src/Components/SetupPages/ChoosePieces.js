139d734074b051b40dbb5c3bca3996229855c109
David Fertitta <Davidj.fertitta@gmail.com>
2022-04-05 10:45:10 -0500

    routes set up in index

--- src/Components/SetupPages/ChoosePieces.js
+++ src/Components/SetupPages/ChoosePieces.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const ChoosePieces = () => {
   return (
     <div className="page-container">
       <h1 className="main-title">Choose Your Piece!</h1>
+      <Link to="/App">PLAY</Link>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./index.css";
 import App from "./App";
+import ChoosePieces from "./Components/SetupPages/ChoosePieces";
+import Landing from "./Components/SetupPages/Landing";
+import "./index.css";
 
 ReactDOM.render(
   <BrowserRouter>
     <React.StrictMode>
-      <App />
+      <Route path="app" element={<App />} />
+      <Route path="choosepieces" element={<ChoosePieces />} />
+      <Route path="/" element={<Landing />} />
     </React.StrictMode>
   </BrowserRouter>,
   document.getElementById("root")
