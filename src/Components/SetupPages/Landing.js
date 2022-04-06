4e5740aa831f9051078e798970b761b632f59ce0
David Fertitta <Davidj.fertitta@gmail.com>
2022-04-06 00:28:46 -0500

    basic layout

--- src/Components/SetupPages/Landing.js
+++ src/Components/SetupPages/Landing.js
@@ -1,11 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logomon from "../../Assets/Landing/logomon.png";
+import monoplaoren from "../../Assets/Landing/monoplaoren.png";
+import "../Styles/Landing.css";
 
 export const Landing = () => {
   return (
     <div className="page-container">
-      <h1 className="main-title">Welcome to Laowai Monopoly!</h1>
-      <Link to="/ChoosePieces">START</Link>
+      <div className="logo-with-man">
+        <div className="logo-and-button">
+          <div className="logo-container">
+            <img className="main-logo" alt="logo" src={logomon} />
+          </div>
+          <div className="button-container">
+            <Link to="choosepieces">
+              <button className="play-button">PLAY</button>
+            </Link>
+          </div>
+        </div>
+        <img className="man" alt="monopoly man" src={monoplaoren} />
+      </div>
     </div>
   );
 };
