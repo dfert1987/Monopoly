15ee9cc09cd454e8709241073771fd01ca70d39b
David Fertitta <Davidj.fertitta@gmail.com>
2022-04-06 08:55:21 -0500

    transition added

--- src/Components/SetupPages/Landing.js
+++ src/Components/SetupPages/Landing.js
@@ -1,26 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import { pageAnimation } from "../../Transitions/Index";
 import logomon from "../../Assets/Landing/logomon.png";
 import monoplaoren from "../../Assets/Landing/monoplaoren.png";
 import "../Styles/Landing.css";
 
 export const Landing = () => {
   return (
-    <div className="page-container">
-      <div className="logo-with-man">
-        <div className="logo-and-button">
-          <div className="logo-container">
-            <img className="main-logo" alt="logo" src={logomon} />
+    <motion.div
+      className="landing"
+      initial="out"
+      animate="end"
+      exit="out"
+      variants={pageAnimation}
+    >
+      <div className="page-container">
+        <div className="logo-with-man">
+          <div className="logo-and-button">
+            <div className="logo-container">
+              <img className="main-logo" alt="logo" src={logomon} />
+            </div>
+            <div className="button-container">
+              <Link to="choosepieces">
+                <button className="play-button">PLAY</button>
+              </Link>
+            </div>
           </div>
-          <div className="button-container">
-            <Link to="choosepieces">
-              <button className="play-button">PLAY</button>
-            </Link>
-          </div>
+          <img className="man" alt="monopoly man" src={monoplaoren} />
         </div>
-        <img className="man" alt="monopoly man" src={monoplaoren} />
       </div>
-    </div>
+    </motion.div>
   );
 };
 
---
+++ src/Transitions/Index.js
@@ -0,0 +1,14 @@
+export const pageAnimation = {
+  in: {
+    opacity: 1,
+    x: -300,
+  },
+  out: {
+    opacity: 0,
+    x: 300,
+  },
+  end: {
+    x: 0,
+    opacity: 1,
+  },
+};
