63a4c1f6da65417d9768509a048cabcda560c806
David Fertitta <Davidj.fertitta@gmail.com>
2022-04-08 12:45:50 -0500

    title and button styled

--- src/Components/SetupPages/ChoosePieces.js
+++ src/Components/SetupPages/ChoosePieces.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../Styles/ChoosePieces.css";
 
 export const ChoosePieces = () => {
   return (
     <div className="page-container">
-      <h1 className="main-title">Choose Your Piece!</h1>
-      <Link to="/App">PLAY</Link>
+      <div className="title-container">
+        <h1 className="main-title">CHOOSE YOUR PIECE</h1>
+      </div>
+      <div className="button-container">
+        <Link to="/App">
+          <button className="play-button">PLAY</button>
+        </Link>
+      </div>
     </div>
   );
 };
