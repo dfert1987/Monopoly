49a6bc4ffc64666e318df0f188cdf60c201506ab
David Fertitta <Davidj.fertitta@gmail.com>
2022-04-10 12:26:01 -0500

    text added to carousel

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -12,6 +12,40 @@ export const CarouselItem = ({ children, width }) => {
 const Carousel = ({ children }) => {
   const [activeIndex, setActiveIndex] = useState(0);
 
+  const setText = () => {
+    if (activeIndex === 0) {
+      return "TRIKE";
+    } else if (activeIndex === 1) {
+      return "YELLOW WEASEL";
+    } else if (activeIndex === 2) {
+      return "BAIJIU";
+    } else if (activeIndex === 3) {
+      return "SLEEPY BAOAN";
+    } else if (activeIndex === 4) {
+      return "BAOZI";
+    } else if (activeIndex === 5) {
+      return "ROAST DUCK";
+    } else if (activeIndex === 6) {
+      return "FU DOG";
+    } else if (activeIndex === 7) {
+      return "JIAOZI";
+    } else if (activeIndex === 8) {
+      return "LANTERN";
+    } else if (activeIndex === 9) {
+      return "MOBIKE";
+    } else if (activeIndex === 10) {
+      return "MORGAN";
+    } else if (activeIndex === 11) {
+      return "POODLE";
+    } else if (activeIndex === 12) {
+      return "TAXI";
+    } else if (activeIndex === 13) {
+      return "TEAPOT";
+    } else if (activeIndex === 14) {
+      return "YANJING";
+    }
+  };
+
   const updateIndex = (newIndex) => {
     if (newIndex < 0) {
       newIndex = 0;
@@ -23,6 +57,7 @@ const Carousel = ({ children }) => {
 
   return (
     <div className="carousel">
+      <h2 className="choice-name">{setText()}</h2>
       <div
         className="inner"
         style={{ transform: `translateX(-${activeIndex * 100}%)` }}
@@ -33,6 +68,7 @@ const Carousel = ({ children }) => {
       </div>
       <div className="indicators">
         <button
+          className="indicators-button"
           onClick={() => {
             updateIndex(activeIndex - 1);
           }}
@@ -40,6 +76,7 @@ const Carousel = ({ children }) => {
           PREV
         </button>
         <button
+          className="indicators-button"
           onClick={() => {
             updateIndex(activeIndex + 1);
           }}
