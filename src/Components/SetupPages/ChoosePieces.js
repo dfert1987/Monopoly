665382498471877910afcc9f72b6c3f6e2ca8a6c
David Fertitta <Davidj.fertitta@gmail.com>
2022-04-16 22:39:20 -0500

    soudns for controls and first two screen all set

--- src/Components/SetupPages/ChoosePieces.js
+++ src/Components/SetupPages/ChoosePieces.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Carousel, { CarouselItem } from "../Carousel";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
