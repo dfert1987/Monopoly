aa0996cfd9425306dd692da820a0eea667ff9028
David Fertitta <Davidj.fertitta@gmail.com>
2022-05-11 12:36:10 -0500

    all map functions in place. some basic styling

--- src/Components/Modals/TradeModal.js
+++ src/Components/Modals/TradeModal.js
@@ -44,31 +44,7 @@ const TradeModal = ({
     getFilteredP2Props();
     getFilteredP2Utils();
     getFilteredP2RRs();
-
-    // let availableP1Props = properties.filter(
-    //   (property) => property.ownedP1 === true && !property.mortgaged
-    // );
-    // let availableP1RRs = railRoads.filter(
-    //   (rr) => rr.ownedP1 === true && !rr.mortgaged
-    // );
-    // let availableP1Utils = utilities.filter(
-    //   (util) => util.ownedP1 === true && !util.mortgaged
-    // );
-    // let availableP2Props = properties.filter(
-    //   (property) => property.ownedP2 === true && !property.mortgaged
-    // );
-    // let availableP2RRs = railRoads.filter(
-    //   (rr) => rr.ownedP2 === true && !rr.mortgaged
-    // );
-    // let availableP2Utils = utilities.filter(
-    //   (util) => util.ownedP2 === true && !util.mortgaged
-    // );
-    // setP1Props(availableP1Props);
-    // setP1RRs(availableP1RRs);
-    // setP1Utils(availableP1Utils);
-    // setP2Props(availableP2Props);
-    // setP2RRs(availableP2RRs);
-    // setP2Utils(availableP2Utils);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [properties, utilities, railRoads]);
 
   const getFilteredP1Props = () => {
@@ -117,7 +93,6 @@ const TradeModal = ({
     click();
   };
 
-  console.log(p1Props);
   const showP1Props = () => {
     if (p1Props && p1Props.length > 0) {
       return p1Props.map((property, index) => {
@@ -130,7 +105,65 @@ const TradeModal = ({
     }
   };
 
-  const showP2Props = () => {};
+  const showP1RRs = () => {
+    if (p1RRs && p1RRs.length > 0) {
+      return p1RRs.map((rr, index) => {
+        return (
+          <div className="container-prop" key={index}>
+            <p className="propname">{rr.Name}</p>
+          </div>
+        );
+      });
+    }
+  };
+
+  const showP1Utils = () => {
+    if (p1Utils && p1Utils.length > 0) {
+      return p1Utils.map((util, index) => {
+        return (
+          <div className="container-prop" key={index}>
+            <p className="propname">{util.Name}</p>
+          </div>
+        );
+      });
+    }
+  };
+
+  const showP2Props = () => {
+    if (p2Props && p2Props.length > 0) {
+      return p2Props.map((property, index) => {
+        return (
+          <div className="container-prop" key={index}>
+            <p className="propname">{property.Name}</p>
+          </div>
+        );
+      });
+    }
+  };
+
+  const showP2RRs = () => {
+    if (p2RRs && p2RRs.length > 0) {
+      return p2RRs.map((rr, index) => {
+        return (
+          <div className="container-prop" key={index}>
+            <p className="propname">{rr.Name}</p>
+          </div>
+        );
+      });
+    }
+  };
+
+  const showP2Utils = () => {
+    if (p2Utils && p2Utils.length > 0) {
+      return p2Utils.map((util, index) => {
+        return (
+          <div className="container-prop" key={index}>
+            <p className="propname">{util.Name}</p>
+          </div>
+        );
+      });
+    }
+  };
 
   return (
     <>
@@ -144,7 +177,7 @@ const TradeModal = ({
             exit="hidden"
           >
             <motion.div
-              className="flex flexColumn innerModalPurchase"
+              className="flex flexColumn innerModalPurchase trade"
               variants={modal}
               initial="hidden"
               animate="visible"
@@ -160,11 +193,25 @@ const TradeModal = ({
                   <h2 className="trade-title">MAKE A DEAL</h2>
                   <div className="trade-columns">
                     <div className="trade-column first-player">
-                      {showP1Props()}
+                      <h3 className="player-title">Player 1</h3>
+                      <div className="properties-container">
+                        {showP1Props()}
+                      </div>
+                      <div className="properties-container">{showP1RRs()}</div>
+                      <div className="properties-container">
+                        {showP1Utils()}
+                      </div>
                     </div>
                     <div className="divider-line"></div>
                     <div className="trade-column second-player">
-                      {showP2Props()}
+                      <h3 className="player-title">Player 2</h3>
+                      <div className="properties-container">
+                        {showP2Props()}
+                      </div>
+                      <div className="properties-container">{showP2RRs()}</div>
+                      <div className="properties-container">
+                        {showP2Utils()}
+                      </div>
                     </div>
                   </div>
                 </div>
