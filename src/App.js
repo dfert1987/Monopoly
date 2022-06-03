f44dc554d2bad8830a0cbd31a21bc00621b6910a
David Fertitta <Davidj.fertitta@gmail.com>
2022-06-03 12:38:50 -0500

    build

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { Properties } from "./Assets/Holdings/Properties";
 import { RailRoads } from "./Assets/Holdings/Railroads";
 import { Utilities } from "./Assets/Holdings/Utilities";
 import { useLocation } from "react-router-dom";
-import { counter } from "@fortawesome/fontawesome-svg-core";
 
 const App = () => {
   const [counterP1, setCounterP1] = useState(1);
